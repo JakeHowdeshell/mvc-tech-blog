60ac948eaf1761756186c5bd7ab6e42821b87306
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-09-17 18:04:22 -0500

    added cookie criteria

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const hbs = exphbs.create({ helpers });
 // need to update sess information accordingly
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
