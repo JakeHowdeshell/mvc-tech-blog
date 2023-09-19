15c7ab56a72ff3279175f7e665d69e3dac7be61a
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-09-19 15:38:37 -0500

    added the route for comments and updated the the homepage to display comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,132 +1,160 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
-const withAuth = require('../utils/auth.js');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth.js");
 
-router.get('/', async (req, res) => {
-    try {
-      const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-      });
-  
-      const posts = postData.map((post) =>
-        post.get({ plain: true })
-      );
-  console.log(posts.id)
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: ["content", "user_id"],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 //  get route for dashboard
-router.get('/dashboard', withAuth,  async (req, res) => {
-    const loggedInUser = req.session.userId;
-    const onDashboard = true;
-    try {
-      const userPostData = await Post.findAll({
-        where: {user_id: loggedInUser},
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-      });
-      const posts = userPostData.map((post) =>
-        post.get({ plain: true })
-      );
-  
-      res.render('dashboard', {
-        posts,
-        loggedIn: req.session.loggedIn,
-        onDashboard,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+router.get("/dashboard", withAuth, async (req, res) => {
+  const loggedInUser = req.session.userId;
+  const onDashboard = true;
+  try {
+    const userPostData = await Post.findAll({
+      where: { user_id: loggedInUser },
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const posts = userPostData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("dashboard", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      onDashboard,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/comment/:id", withAuth, async (req, res) => {
+  const loggedInUser = req.session.userId;
+  try {
+    const userPostData = await Post.findByPk(req.params.id, {
+      where: { user_id: loggedInUser },
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: ["content", "user_id"],
+        },
+      ],
+    });
+    const posts = userPostData.get({ plain: true });
+
+    console.log(posts);
+    res.render("comments", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 //   need to add a post route for dashboard
-router.post('/dashboard', withAuth, async (req, res) => {
-    try {
-      const newPost = await Post.create(req.body);
-  
-      res.status(200).json(newPost);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+// router.post('/dashboard', withAuth, async (req, res) => {
+//     try {
+//       const newPost = await Post.create(req.body);
+
+//       res.status(200).json(newPost);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
 // need to add a put route for dashboard
-router.put('/dashboard', async (req, res) => {
-    try {
-      const newPost = await Post.update({
-        
-      });
-  
-      res.status(200).json(newPost);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+// router.put('/dashboard', async (req, res) => {
+//     try {
+//       const newPost = await Post.update({
+
+//       });
+
+//       res.status(200).json(newPost);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
 // need to add a delete route for dashboard
-router.delete('/dashboard', async (req, res) => {
-    try {
-      const projectData = await Post.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!projectData) {
-        res.status(404).json({ message: 'No project found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(projectData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-// need to add a put route for the homepage to add comments
-router.post('/comment', withAuth, async (req, res) => {
-    try {
-        const { content, post_id } = req.body;
-      const addComment = await Comment.create({
-        content,
-         user_id: req.session.userId,
-         post_id: post_id,
-      });
-  
-      res.status(201).json(addComment);
-    } catch (err) {
-console.log(err);
-      res.status(500).json(err);
-    }
-  });
+// router.delete('/dashboard', async (req, res) => {
+//     try {
+//       const projectData = await Post.destroy({
+//         where: {
+//           id: req.params.id,
+//           user_id: req.session.user_id,
+//         },
+//       });
 
-  router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/dashboard');
-      return;
-    }
-  
-    res.render('login');
-  });
+//       if (!projectData) {
+//         res.status(404).json({ message: 'No project found with this id!' });
+//         return;
+//       }
 
-  router.get('/sign-up', (req, res) => {
-  
-    res.render('sign-up');
-  });
+//       res.status(200).json(projectData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+//  put route for the homepage to add comments
+router.post("/comment", withAuth, async (req, res) => {
+  try {
+    const { content, post_id } = req.body;
+    const addComment = await Comment.create({
+      content,
+      user_id: req.session.userId,
+      post_id: post_id,
+    });
 
-  module.exports = router;
+    res.status(201).json(addComment);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+// get route for login
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+// get route for sign up
+router.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
+
+module.exports = router;
